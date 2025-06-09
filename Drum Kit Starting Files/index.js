2675bf585b97ab8af2995fd42c3aebfe3dbac38e
quirkyharsh <harshpatil3.1428@gmail.com>
2025-06-09 15:25:29 +0530

    signing off

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,40 @@
+
+for (var i = 0; i < document.querySelectorAll("button").length; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function() {
+    
+    var buttoninnerHTML = this.innerHTML;
+    
+    switch (buttoninnerHTML) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-5.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/tom-6.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/tom-7.mp3");
+            audio.play();
+            break;
+    }
+});
+}
+
+
